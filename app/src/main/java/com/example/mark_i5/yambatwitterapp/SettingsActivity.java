1b496579aa4ca5a24e15508fe0aa1415bf8fb4b5
Mark Lloyd <mark.g.n.lloyd@gmail.com>
2014-08-19 22:55:22 +0100

    first commit

---
+++ app/src/main/java/com/example/mark_i5/yambatwitterapp/SettingsActivity.java
@@ -0,0 +1,23 @@
+package com.example.mark_i5.yambatwitterapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+
+public class SettingsActivity extends PreferenceActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return super.isValidFragment(fragmentName);
+    }
+}
